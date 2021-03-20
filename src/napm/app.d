c458bb2c73a23e31c74d623a499784873ab0afd1
Johannes Teichrieb <isrvoid@gmail.com>
2021-03-20 01:30:36 +0100

    Partially implement client; refactor

--- src/napm/app.d
+++ src/napm/app.d
@@ -4,9 +4,11 @@
  */
 
 module napm.app;
+
 import std.path : buildPath;
-import std.file;
 import std.exception : enforce;
+import std.file;
+import std.stdio : File, writeln;
 import std.format : format;
 
 import napm.tags;
@@ -14,10 +16,10 @@ import napm.tags;
 void main(string[] args)
 {
     import std.getopt;
-    bool isNewTag, isTagLine;
+    bool isAddTag, isTagLine;
     auto opt = getopt(args,
             config.passThrough,
-            "add|a", "Add new tag to napm_tags.", &isNewTag,
+            "add|a", "Add new tag to napm_tags.", &isAddTag,
             "tag|t", "Use tag ignoring napm_tags. Intended for one-off use.", &isTagLine
             );
 
@@ -31,17 +33,63 @@ void main(string[] args)
         return;
     }
 
-    if (isNewTag)
+    args = args[1 .. $];
+    const isStartDaemon = !(isAddTag || isTagLine || args.length);
+    Tag tag;
+    if (isAddTag || isTagLine)
     {
-        const tag = parseTag(args[1 .. $]);
-        writeNewTag(tag);
+        tag = parseTag(args);
+        args.length = 0;
+    }
 
+    if (isAddTag)
+    {
+        writeNewTag(tag);
         if (!isTagLine)
             return;
     }
+
+    const isRunning = isDaemonRunning();
+    if (isStartDaemon && isRunning)
+    {
+        writeln("daemon already running");
+        return;
+    }
+
+    if (!isRunning)
+    {
+        if (!isStartDaemon)
+            writeln("daemon not running");
+
+        runDaemon();
+        return;
+    }
+
+    if (args.length > 1)
+        throw new Exception("Single tag name argument allowed");
+
+    if (args.length)
+        tag = loadTag(args[0]);
+
+    sendTag(tag);
 }
 
 @safe:
+
+bool isDaemonRunning()
+{
+    import std.string : strip;
+    try
+    {
+        const sPid = buildPath(tempDir(), "napm", "pid").readText.strip;
+        const processName = buildPath("/proc", sPid, "comm").readText.strip;
+        return processName == "napm";
+    }
+    catch (Exception)
+    {
+        return false;
+    }
+}
 
 Tag parseTag(string[] args)
 {
@@ -51,27 +99,50 @@ Tag parseTag(string[] args)
 }
 
 const string configDir;
+const string tagsPath;
 static this()
 {
     import std.process : environment;
     const home = environment.get("HOME");
     enforce(home, "HOME not set");
     configDir = buildPath(home, ".config", "napm");
+    tagsPath = buildPath(configDir, "napm_tags");
+}
+
+auto loadTag(string name)
+{
+    auto file = File(tagsPath, "r");
+    auto search = (() @trusted => file.byLine.findTag(name))();
+    enforce(!search.isNull, format!"Tag '%s' not found in '%s'"(name, tagsPath));
+    return search.get;
 }
 
 void writeNewTag(Tag tag)
 {
     import std.array : join;
-    import std.stdio : File;
     if (!exists(configDir))
         mkdirRecurse(configDir);
 
-    enum fileName = "napm_tags";
-    auto file = File(buildPath(configDir, fileName), "a+");
+    auto file = File(tagsPath, "a+");
     const isFileEmpty = !file.size;
     auto isNewTag = () @trusted => isFileEmpty || file.byLine.findTag(tag.name).isNull;
-    enforce(isNewTag(), format!"Tag '%s' already exists in %s"(tag.name, fileName));
+    enforce(isNewTag(), format!"Tag '%s' already exists in '%s'"(tag.name, tagsPath));
     if (!isFileEmpty)
         file.writeln();
     file.writeln(tag.toLine);
+    writeln("tag added");
+}
+
+void runDaemon()
+{
+    // FIXME
+    writeln("starting daemon");
+    // input readable?
+    // crc?
+}
+
+void sendTag(Tag tag)
+{
+    // FIXME
+    writeln("armed");
 }
